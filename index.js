236f12b2ef804e999aee2294c639f2e9caccf5cd
yassine <y.zaoui.pro@gmail.com>
2022-11-23 15:45:37 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const bubble = document.createElement("span");
+bubble.classList.add("bubble");
+document.body.appendChild(bubble);
+
+const size = Math.random() * 200 + 100 + "px";
+bubble.style.height = size;
+bubble.style.width = size;
+console.log(size);
+
+bubble.style.top = Math.random() * 100 + 50 + "%";
+bubble.style.left = Math.random() * 100 + "%";
